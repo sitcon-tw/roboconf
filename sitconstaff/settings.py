196e137db0a459b74cfd1cbd549877c9400e7351
Poren Chiang <ren.chiang@gmail.com>
2013-07-13 01:55:39 +0800

    Adjust config for deploying to Heroku

--- sitconstaff/settings.py
+++ sitconstaff/settings.py
@@ -156,3 +156,24 @@ LOGGING = {
         },
     }
 }
+
+# Heroku-specific settings
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
--- sitconstaff/wsgi.py
+++ sitconstaff/wsgi.py
@@ -25,7 +25,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitconstaff.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
