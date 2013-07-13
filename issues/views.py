ba9507567fbbe11ea8f77a6f62ae92e608fa2052
Poren Chiang <ren.chiang@gmail.com>
2013-07-13 15:14:02 +0800

    Add missing import and fine tune view

--- issues/views.py
+++ issues/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
 from issues.models import Issue
 
