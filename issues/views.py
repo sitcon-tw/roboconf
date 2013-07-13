61e92626e29fff78290a3daffad80f25fff8b0ba
Poren Chiang <ren.chiang@gmail.com>
2013-07-14 02:53:42 +0800

    Add filters to view

--- issues/views.py
+++ issues/views.py
@@ -5,12 +5,43 @@ from django.views import generic
 
 from issues.models import Issue
 
-class ListView(generic.ListView):
-	template_name = 'issues_list.html'
-	context_object_name = 'issues'
+order_mapping = { 'created': 'creation_time', 'due': 'due_time' }
 
-	def get_queryset(self):
-		return Issue.objects.order_by('id')[:10]
+def list(request, dataset=None, action='list'):
+	context = list_context()
+	if not dataset:
+		dataset = Issue.objects.all()
+
+	# Filter
+	state = not (request.GET.get('state') == 'closed')
+	dataset.filter(is_open=state)
+
+	label = request.GET.get('label')
+	if label and str(label).isdigit():
+		dataset.filter(labels__pk=label)
+
+	# Sort
+	direction = (request.GET.get('direction') == 'asc')
+	sorting = request.GET.get('sort', 'created')
+	dataset.order_by(('' if direction else '-') + order_mapping[sorting])
+
+	context['current_url'] = request.path 
+	context['issues'] = dataset
+	context['sorting'] = sorting
+	context['is_asc'] = direction
+	context['is_open'] = state
+	context['mode'] = action
+	return render(request, 'issues_list.html', context)
+
+def assigned(request, user_id):
+	return list(request, dataset=Issue.objects.filter(assignee__pk=user_id), action='assigned')
+
+def created(request, user_id):
+	return list(request, dataset=Issue.objects.filter(creator__pk=user_id), action='created')
+
+def starred(request, user_id):
+	# TODO: Implement "starring" (a.k.a watch issue)
+	return HttpResponseRedirect(reverse('issues:list'))
 
 class DetailView(generic.DetailView):
 	model = Issue
