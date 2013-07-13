f5885bc637913c71adc0a3000e5873cae07352ac
Poren Chiang <ren.chiang@gmail.com>
2013-07-14 04:23:08 +0800

    Add missing closing tag and further work on UI

--- users/templatetags/users.py
+++ users/templatetags/users.py
@@ -4,6 +4,6 @@ import md5
 register = template.Library()
 
 @register.filter(is_safe=True)
-def get_avatar(value):
+def get_avatar(value, size=None):
 	hash_value = md5.new(value.strip().lower()).hexdigest()
-	return 'https://secure.gravatar.com/avatar/%s?s=144&d=retro' % hash_value
+	return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value) + (('&s=%s' % size) if size else '')
