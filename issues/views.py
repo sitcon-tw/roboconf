3ae9f37512fa355832ea11b68325a34612f612ad
Poren Chiang <ren.chiang@gmail.com>
2013-07-13 14:26:24 +0800

    Add useable views.

--- issues/views.py
+++ issues/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from issues.models import Issue
 
 def list(request):
 	issues = Issue.objects.order_by('+id')[:10]
-	context = { 'issues': issues }
-    return render(request, 'issues/list.html', context)
+    return render(request, 'issues/list.html', { 'issues': issues })
     
 def create(request):
-	return HttpResponse('Create issue')
+	return render(request, 'issues/create.html', {})
 
 def detail(request, id):
-	try:
-		issue = Issue.objects.get(pk=id)
-	except Issue.DoesNotExist:
-		raise Http404
+	issue = get_object_or_404(Issue, pk=id)
 	return render(request, 'issues/detail.html', { 'issue': issue })
