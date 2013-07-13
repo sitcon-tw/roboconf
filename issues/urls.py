89586663e53128edc48463d329e1438b65cc63f8
Poren Chiang <ren.chiang@gmail.com>
2013-07-13 14:04:52 +0800

    Create issue URL views

--- issues/urls.py
+++ issues/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from issues import views
 
 urlpatterns = patterns('',
-		url(r'^$', views.index, name='index')
+		url(r'^$', views.list, name='list'),
+		url(r'^new$', views.create, name='create'),
+		url(r'^(?P<id>\d+)$', views.detail, name='detail')
 	)
--- issues/views.py
+++ issues/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
-def index(request):
+def list(request):
     return HttpResponse('Issue Tracker')
     
+def create(request):
+	return HttpResponse('Create issue')
+
+def detail(request, id):
+	return HttpResponse('Issue #%s' % id)
