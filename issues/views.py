d823bbec13f8625f7d357282fea051746b66a9f8
Poren Chiang <ren.chiang@gmail.com>
2013-07-13 14:13:35 +0800

    Add template

--- issues/views.py
+++ issues/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from issues.models import Issue
 
 def list(request):
-    return HttpResponse('Issue Tracker')
+	issues = Issue.objects.order_by('+id')[:10]
+	template = loader.get_template('issues/list.html')
+	context = RequestContext(request, {
+			'issues': issues,
+		})
+    return HttpResponse(template.render(context))
     
 def create(request):
 	return HttpResponse('Create issue')
