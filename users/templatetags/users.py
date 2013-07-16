339a2bbb910619aca148ba465a5d42cb8dee93ac
Poren Chiang <ren.chiang@gmail.com>
2013-07-17 01:30:41 +0800

    Fix username display issue

--- users/templatetags/users.py
+++ users/templatetags/users.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.auth.models import User
 import md5
 
 register = template.Library()
@@ -9,3 +10,11 @@ def get_avatar(value, size=None):
 		size = 144
 	hash_value = md5.new(value.strip().lower()).hexdigest()
 	return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value) + (('&s=%s' % size) if size else '')
+
+@register.filter(is_safe=True)
+def get_username(id):
+	try:
+		user = User.objects.get(id=id)
+		return user.username
+	except User.DoesNotExist:
+		return id
