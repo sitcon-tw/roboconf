57a3e29c6366e5e55da8940a8d1ce8c83a565716
Poren Chiang <ren.chiang@gmail.com>
2013-07-17 11:01:08 +0800

    Mark markdown output safe

--- core/templatetags/markdown.py
+++ core/templatetags/markdown.py
@@ -1,11 +1,12 @@
 from django import template
+from django.utils.safestring import mark_safe
 import misaka as m
 
 register = template.Library()
 
-@register.filter(is_safe=True)
-def markdown(value):
-	return m.html(value, 
+@register.filter(needs_autoescape=True)
+def markdown(value, autoescape=None):
+	return mark_safe(m.html(value, 
 		extensions=m.EXT_FENCED_CODE | m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.EXT_SPACE_HEADERS,
-		render_flags=m.HTML_SKIP_HTML,
-	)
+		render_flags=(m.HTML_ESCAPE if autoescape else None),
+	))
