5ac55251874c083873c4bb374f2f603128c115f9
Poren Chiang <ren.chiang@gmail.com>
2013-07-17 10:43:37 +0800

    Add markdown support

---
+++ core/templatetags/markdown.py
@@ -0,0 +1,11 @@
+from django import template
+import misaka as m
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def markdown(value):
+	return m.html(value, 
+		extensions=m.EXT_FENCED_CODE | m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.EXT_SPACE_HEADERS,
+		render_flags=HTML_ESCAPE,
+	)
