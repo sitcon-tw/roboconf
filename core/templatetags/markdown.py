113fb6ac1e7e803676ef5db996f091ff6ddaf15c
Poren Chiang <ren.chiang@gmail.com>
2013-07-17 10:49:19 +0800

    Fix stupid name error

--- core/templatetags/markdown.py
+++ core/templatetags/markdown.py
@@ -7,5 +7,5 @@ register = template.Library()
 def markdown(value):
 	return m.html(value, 
 		extensions=m.EXT_FENCED_CODE | m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.EXT_SPACE_HEADERS,
-		render_flags=HTML_ESCAPE,
+		render_flags=m.HTML_ESCAPE,
 	)
