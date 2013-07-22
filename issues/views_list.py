b48419f42cbd38ba071da7a8e21e6fa91e2fe9ad
Poren Chiang <ren.chiang@gmail.com>
2013-07-23 03:31:29 +0800

    Add count to issue list

--- issues/views_list.py
+++ issues/views_list.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from issues.models import Label
+from issues.models import *
 
 order_mapping = { 'created': 'creation_time', 'due': 'due_time' }
 
-def list(request, dataset, mode):
-	# Filter
+def datasets():
+	return {
+			'all': Issue.objects,
+			'assigned': Issue.objects.filter(assignee__pk=user_id),
+			'created': Issue.objects.filter(creator__pk=user_id),
+			#'starred': ,
+		}
+
+def list(request, mode):
+	dataset = datasets()[mode]
+	counts = [s.count() for s in dataset]
+
 	is_open = not (request.GET.get('state') == 'closed')
+	counts['open'] = dataset.filter(is_open=True).count()
+	counts['closed'] = dataset.filter(is_open=False).count()
 	dataset = dataset.filter(is_open=is_open)
 
 	label = request.GET.get('label')
@@ -14,15 +26,17 @@ def list(request, dataset, mode):
 
 	# Sort
 	is_asc = (request.GET.get('direction') == 'asc')
-	sorting = request.GET.get('sort', 'created')
-	dataset = dataset.order_by(('' if is_asc else '-') + order_mapping[sorting])
+	sort_order = request.GET.get('sort', 'created')
+	dataset = dataset.order_by(('' if is_asc else '-') + order_mapping[sort_order])
+
+	filters = { 'is_open': is_open, 'mode': mode }
+	sorting = { 'asc': is_asc, 'desc': not is_asc, 'order': sorting }
 
 	return render(request, 'issues_list.html', {
 		'current_url': request.path,
 		'issues': dataset,
 		'labels': Label.objects.all(),
+		'filters': filters,
 		'sorting': sorting,
-		'is_asc': is_asc,
-		'is_open': is_open,
-		'mode': mode,
+		'counts': counts,
 	})
