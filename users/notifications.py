858572572e641bbadabf975b6a001714d7506c23
Poren Chiang <ren.chiang@gmail.com>
2013-07-24 02:40:03 +0800

    Add notifications code

---
+++ users/notifications.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import email, smtplib
+import os
+
+def send_mail(user, subject, content):
+	message = email.mime.text.MIMEText(content, 'html')
+
+	client = smtplib.SMTP(os.environ['EMAIL_SERVER'], os.environ['EMAIL_PORT'])
+	client.login(os.environ['EMAIL_USER'], os.environ['EMAIL_PASSWORD'])
+	client.sendmail('"SITCON 行政系統" <staff@sitcon.org>',
+					'"%s" <%s>' % (user.username, user.email),
+					message.as_string())
+	client.quit()
+
+def send_sms(user, content):
+	# TODO: implement SMS
+	pass
