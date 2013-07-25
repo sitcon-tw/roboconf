eb54671bee84d4c31fb4c094304ccdfa8205cd4e
Poren Chiang <ren.chiang@gmail.com>
2013-07-25 13:30:42 +0800

    Fix layout and show profile

--- users/views_list.py
+++ users/views_list.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import User, Group
 from users.models import *
 
 def list(request):
-	return render(request, 'users_list.html', { 'users': User.objects.all(), 'groups': Group.objects.all() })
+	return render(request, 'users_list.html', {
+		'users': User.objects.order_by('username').all(),
+		'groups': Group.objects.order_by('name').all(),
+	})
--- users/views_profile.py
+++ users/views_profile.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from users.models import *
 
 def profile(request, id):
-	return render(request, 'users_profile.html', {})
+	u = get_object_or_404(User, pk=id)
+	return render(request, 'users_profile.html', {
+		'u': u,
+	})
