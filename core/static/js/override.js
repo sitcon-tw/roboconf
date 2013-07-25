fb26d024ad4cca91efc7cdcb4d6e480ac4882004
Poren Chiang <ren.chiang@gmail.com>
2013-07-25 13:06:35 +0800

    Add autogrow textarea

---
+++ core/static/js/override.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("textarea").keyup(function(e) {
+		var $t = $(this);
+		var from = $t.outerHeight();
+		var dest = this.scrollHeight + 
+			parseFloat($t.css("border-top-width") || 0) + parseFloat($t.css("border-bottom-width") || 0) +
+			parseInt(parseFloat($($0).css("font-size"))*1.15);
+		if (from < dest) $t.height(from + (dest - from));
+	});
+});
