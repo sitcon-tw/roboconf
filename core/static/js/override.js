2283ef866ecf2c70ba01bb8a922b63ac429851f5
Poren Chiang <ren.chiang@gmail.com>
2013-07-25 13:18:41 +0800

    Fix JS bugs in include, also updating links and CSS

--- core/static/js/override.js
+++ core/static/js/override.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		var from = $t.outerHeight();
 		var dest = this.scrollHeight + 
 			parseFloat($t.css("border-top-width") || 0) + parseFloat($t.css("border-bottom-width") || 0) +
-			parseInt(parseFloat($($0).css("font-size"))*1.15);
+			parseInt(parseFloat($t.css("font-size"))*1.15);
 		if (from < dest) $t.height(from + (dest - from));
 	});
 });
