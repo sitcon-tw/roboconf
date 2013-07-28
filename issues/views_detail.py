fddfdb1db443301a9f9f575c268b6d1e6be76922
Poren Chiang <ren.chiang@gmail.com>
2013-07-29 04:25:24 +0800

    Fix notify bad code

--- issues/views_detail.py
+++ issues/views_detail.py
@@ -24,12 +24,12 @@ def assign(issue, request):
 				issue.assignee = u
 				issue.starring.add(u)			# Automatic starring
 				update(issue=issue, user=request.user, mode=IssueHistory.ASSIGN, content=assignee)
-				notify(issue, user, '* %s 已將此議題指派給 %s *' % (request.user.username, u.username))
+				notify(issue, request.user, '* %s 已將此議題指派給 %s *' % (request.user.username, u.username))
 			except User.DoesNotExist: pass		# Just in case we're under attack...
 		else:
 			issue.assignee = None
 			update(issue=issue, user=request.user, mode=IssueHistory.UNASSIGN)
-			notify(issue, user, '* %s 已撤銷此議題指派的人員*' % (request.user.username))
+			notify(issue, request.user, '* %s 已撤銷此議題指派的人員*' % (request.user.username))
 
 def set_label(issue, request):
 	old_labels = [l.id for l in issue.labels.all()]
@@ -70,18 +70,18 @@ def set_label(issue, request):
 		body.append('套用標籤* ')
 		body.append('、'.join([('「%s」' % l.name) for l in labels_to_add]))
 
-	notify(issue, user, ''.join(body))
+	notify(issue, request.user, ''.join(body))
 
 def comment(issue, request):
 	content = request.POST.get('content')
 	if content:
 		update(issue=issue, user=request.user, content=content)
-		notify(issue, user, content)
+		notify(issue, request.user, content)
 
 def toggle_state(issue, request):
 	issue.is_open = not issue.is_open
 	update(issue=issue, user=request.user, mode=(IssueHistory.REOPEN if issue.is_open else IssueHistory.CLOSE))
-	notify(issue, user, ('* %s 已將此議題結案 *' if issue.is_open else '* %s 已對此議題提出復議 *') % (request.user.username))
+	notify(issue, request.user, ('* %s 已將此議題結案 *' if issue.is_open else '* %s 已對此議題提出復議 *') % (request.user.username))
 
 def toggle_star(issue, request):
 	if issue.starring.filter(id=request.user.id).count():
