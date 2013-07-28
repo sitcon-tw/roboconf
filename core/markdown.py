53c9c77ddf77919540d45f10ecf693edb1f0b8ab
Poren Chiang <ren.chiang@gmail.com>
2013-07-29 02:39:35 +0800

    Fix code copying error

--- core/markdown.py
+++ core/markdown.py
@@ -1,7 +1,7 @@
 import misaka as m
 
 def get_markdown(text, autoescape=True):
-	return m.html(value, 
+	return m.html(text, 
 		extensions=m.EXT_FENCED_CODE | m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.EXT_SPACE_HEADERS,
 		render_flags=(m.HTML_ESCAPE if autoescape else None),
 	)
