356acbb15612f962f2ae1b2f5f9041d29600ae16
Poren Chiang <ren.chiang@gmail.com>
2013-07-29 04:06:47 +0800

    Apply notification to all issue changes

--- issues/views_detail.py
+++ issues/views_detail.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from django.utils import timezone
 from issues.models import *
@@ -8,18 +9,27 @@ def update(issue, user, content='', mode=IssueHistory.COMMENT):
 	issue.save()
 	IssueHistory.objects.create(issue=issue, user=user, mode=mode, content=content)
 
+def notify(issue, user, content):
+	message_subject = 'Re: [#%s] %s' % (issue.id, issue.title)
+	for watcher in issue.starring.all():
+		if user == watcher: continue
+		Message.create_from_user(user, watcher, message_subject, content)
 
 def assign(issue, request):
 	assignee = request.POST.get('assignee')
 	if assignee is not None:					# empty string => unassign
 		if len(assignee) > 0:
 			try:
-				issue.assignee = User.objects.get(id=assignee)
+				u = User.objects.get(id=assignee)
+				issue.assignee = u
+				issue.starring.add(u)			# Automatic starring
 				update(issue=issue, user=request.user, mode=IssueHistory.ASSIGN, content=assignee)
+				notify(issue, user, '* %s 已將此議題指派給 %s *' % (request.user.username, u.username))
 			except User.DoesNotExist: pass		# Just in case we're under attack...
 		else:
 			issue.assignee = None
 			update(issue=issue, user=request.user, mode=IssueHistory.UNASSIGN)
+			notify(issue, user, '* %s 已撤銷此議題指派的人員*' % (request.user.username))
 
 def set_label(issue, request):
 	old_labels = [l.id for l in issue.labels.all()]
@@ -31,13 +41,16 @@ def set_label(issue, request):
 		except ValueError: pass
 
 	# Remove unused labels
+	labels_to_remove = [l for l in old_labels if l not in new_labels]
+	labels_to_add = [l for l in new_labels if l not in old_labels]
+
 	# * Old labels won't have integrity issues so eliminate try block
-	for label_id in [l for l in old_labels if l not in new_labels]:
+	for label_id in labels_to_remove:
 		issue.labels.remove(Label.objects.get(id=label_id))
 		update(issue=issue, user=request.user, mode=IssueHistory.UNLABEL, content=label_id)
 
 	# Add new labels
-	for label_id in [l for l in new_labels if l not in old_labels]:
+	for label_id in labels_to_add:
 		try:
 			issue.labels.add(Label.objects.get(id=label_id))
 			update(issue=issue, user=request.user, mode=IssueHistory.LABEL, content=label_id)
@@ -46,18 +59,29 @@ def set_label(issue, request):
 
 	issue.save()
 
+	body = ['* %s 已為此議題']
+	if len(labels_to_remove) > 0:
+		body.append('移除標籤* ')
+		body.append('、'.join([('「%s」' % l.name) for l in labels_to_remove]))
+		if len(labels_to_add) > 0:
+			body.append(' *，同時')
+
+	if len(labels_to_add) > 0:
+		body.append('套用標籤* ')
+		body.append('、'.join([('「%s」' % l.name) for l in labels_to_add]))
+
+	notify(issue, user, ''.join(body))
+
 def comment(issue, request):
 	content = request.POST.get('content')
 	if content:
 		update(issue=issue, user=request.user, content=content)
-
-		message_subject = 'Re: [#%s] %s' % (issue.id, issue.title)
-		for watcher in issue.starring.all():
-			Message.create_from_user(request.user, watcher, message_subject, content)
+		notify(issue, user, content)
 
 def toggle_state(issue, request):
 	issue.is_open = not issue.is_open
 	update(issue=issue, user=request.user, mode=(IssueHistory.REOPEN if issue.is_open else IssueHistory.CLOSE))
+	notify(issue, user, ('* %s 已將此議題結案 *' if issue.is_open else '* %s 已對此議題提出復議 *') % (request.user.username))
 
 def toggle_star(issue, request):
 	if issue.starring.filter(id=request.user.id).count():
--- notifications/management/commands/syncmail.py
+++ notifications/management/commands/syncmail.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from django.core.management.base import NoArgsCommand
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
