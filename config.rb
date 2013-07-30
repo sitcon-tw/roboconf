496e1e2f22ea2795c8c9ac63a50636159d34f28d
Poren Chiang <ren.chiang@gmail.com>
2013-07-31 02:23:03 +0800

    Introduced Compass as CSS minifier

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "core/static/css"
+sass_dir = "core/static/scss"
+images_dir = "core/static/img"
+javascripts_dir = "core/static/js"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass core/static/scss scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
