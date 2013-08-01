ff44f5a80f2fdc4d45887e21d18be7c189239f6f
Poren Chiang <ren.chiang@gmail.com>
2013-08-01 22:07:04 +0800

    Implement password changing backend

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.debug import sensitive_post_parameters, sensitive_variables
 import django.contrib.auth as auth
 
 import views_list as _list
@@ -46,6 +46,19 @@ def profile(request, id):
 	return _profile.profile(request, id)
 
 @login_required
+@sensitive_variables()
 @sensitive_post_parameters()
 def change_password(request):
-	return render(request, 'users_change_password.html')
+	status = ''
+	old_password = request.POST.get('old_password')
+	new_password = request.POST.get('new_password')
+	verify_password = request.POST.get('verify_password')
+	if old_password and new_password and verify_password:
+		if request.user.check_password(old_password):
+			if new_password == verify_password:
+				request.user.set_password(new_password)
+				request.user.save()
+				status = 'success'
+			else: status = 'password_mismatch'
+		else: status = 'invalid_login'
+	return render(request, 'users_change_password.html', {'status': status})
