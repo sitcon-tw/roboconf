74b2525dd2d30b6a8812077ccd2e7569d3884d15
Poren Chiang <ren.chiang@gmail.com>
2013-08-02 17:19:41 +0800

    Minor bug fix

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.debug import sensitive_post_parameters, sensitive_variables
