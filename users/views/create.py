6673ce550ece2e058ae47e9646b5b6fafb9eee0e
Poren Chiang <ren.chiang@gmail.com>
2013-09-03 12:27:52 +0800

    Fix missing parameter 'category', also some indent fix

--- users/views/create.py
+++ users/views/create.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.models import Group
+from users.models import GroupCategory
 
 @permission_required('auth.add_user', login_url='users:list')
 def create(request):
 	return render(request, 'users_create.html', {
-		'groups': Group.objects.all(),
+		'categories': GroupCategory.objects.all(),
 	})
