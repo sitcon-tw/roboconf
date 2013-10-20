7791b443bd00837cf53bc1c96b315f86c098fed5
Poren Chiang <ren.chiang@gmail.com>
2013-10-20 23:36:21 +0800

    Fix missing import

--- users/views/edit.py
+++ users/views/edit.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from users.models import *
