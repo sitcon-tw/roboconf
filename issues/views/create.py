a35c3647b0aa2a0173b01e681ab4ff8fbf2cc2a9
Poren Chiang <ren.chiang@gmail.com>
2013-10-31 19:22:47 +0800

    Moved issues views to separate directory. Needs to rework current form POST model to ajax API

---
+++ issues/views/create.py
@@ -0,0 +1,72 @@
+from django.contrib.auth.models import User
+from django.utils import dateparse
+from issues.models import *
+from issues.utils import send_mail
+import datetime
+
+@login_required
+def create(request):
+	if 'submit' in request.POST:
+		if not request.user.has_perm('issues.add_issue'):
+			return redirect(reverse('issues:list'))	# Audit fail
+
+		errors = []
+		
+		issue = Issue()
+		issue.title = request.POST['title']
+		issue.content = request.POST['content']
+		issue.creator = request.user
+
+		due_time = request.POST.get('due_time', '').strip()
+		if len(due_time) > 0:
+			try:
+				if len(due_time) <= 10:
+					due_time = dateparse.parse_date(due_time)
+					due_time = datetime.datetime.combine(due_time, datetime.time()) if due_time else None
+				else:
+					due_time = dateparse.parse_datetime(due_time)
+			except ValueError:
+				errors.append('date-invalid')
+
+			if due_time: issue.due_time = due_time
+			else:
+				errors.append('date-misformed')
+
+		assignee = request.POST.get('assignee')
+		if assignee:	# Empty value => not assigned, no need to set
+			try:
+				assignee = request.POST.get('assignee')
+				issue.assignee = User.objects.get(id=assignee)
+			except User.DoesNotExist:
+				assignee = None		# Just in case we're under attack...
+
+		if len(errors) < 1:
+			issue.save()	# Need to save before we can enforce N to N relationship
+			issue.starring.add(request.user)	# Auto watch
+
+			if assignee:
+				IssueHistory.objects.create(issue=issue, user=request.user,
+											mode=IssueHistory.ASSIGN, content=assignee)
+
+				issue.starring.add(issue.assignee)	# Auto watch
+				send_mail(request.user, issue.assignee, 'mail/issue_assigned.html', {'issue': issue, 'new_topic': True})
+
+			if due_time:
+				IssueHistory.objects.create(issue=issue, user=request.user, 
+											mode=IssueHistory.SET_DUE, content=due_time)
+
+
+			# Add or remove labels has history so we don't worry on history creation
+			for label_id in request.POST.getlist('labels'):
+				try:
+					issue.labels.add(Label.objects.get(id=label_id))
+				except Label.DoesNotExist: pass
+
+			issue.save()	# Now save the labels
+			return redirect(reverse('issues:detail', args=(issue.id,)))
+		
+	return render(request, 'issues_create.html', {
+		'labels': Label.objects.all(),
+		'users': User.objects.all(),
+		'errors': errors,
+	})
---
+++ issues/views/detail.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from issues.models import *
+from issues.utils import send_mail
+from users.utils import get_user_name
+
+@login_required
+def detail(request, issue_id):
+	issue = get_object_or_404(Issue, pk=issue_id)
+
+	action = request.POST.get('action')		# Check if postback
+	if action == 'assign':
+		assign(issue, request)
+	elif action == 'set-label':
+		set_label(issue, request)
+	#elif action == 'set-due':
+		#pass
+	elif action == 'toggle-star':
+		toggle_star(issue, request)
+	elif action:
+		content = request.POST.get('content')
+		if content: comment(issue, request)
+		if action == 'toggle-state':
+			toggle_state(issue, request)
+
+	return render(request, 'issues_detail.html', {
+		'issue': issue,
+		'labels': Label.objects.all(),
+		'users': User.objects.all(),
+		'has_starred': issue.starring.filter(id=request.user.id).count() > 0,
+	})
+
+
+def update(issue, user, content='', mode=IssueHistory.COMMENT):
+	issue.last_updated = timezone.now()
+	issue.save()
+	IssueHistory.objects.create(issue=issue, user=user, mode=mode, content=content)
+
+def notify(issue, user, template_name, context):
+	for watcher in issue.starring.all():
+		#if user == watcher: continue
+		send_mail(user, watcher, template_name, context)
+
+def assign(issue, request):
+	if not request.user.has_perm('issues.assign_issue'):
+		return	# Audit fail
+		
+	assignee = request.POST.get('assignee')
+	if assignee is not None:					# empty string => unassign
+		if len(assignee) > 0:
+			try:
+				u = User.objects.get(id=assignee)
+				issue.assignee = u
+				issue.starring.add(u)			# Automatic starring
+				update(issue=issue, user=request.user, mode=IssueHistory.ASSIGN, content=assignee)
+				notify(issue, request.user, 'mail/issue_assigned.html', { 'issue': issue })
+			except User.DoesNotExist: pass		# Just in case we're under attack...
+		else:
+			issue.assignee = None
+			update(issue=issue, user=request.user, mode=IssueHistory.UNASSIGN)
+			notify(issue, request.user, 'mail/issue_assigned.html', { 'issue': issue })
+
+def set_label(issue, request):
+	if not (issue.assignee == request.user or request.user.has_perm('issues.label_issue')):
+		return	# Audit fail
+		
+	old_labels = [l.id for l in issue.labels.all()]
+	new_labels = []
+
+	for label_str in request.POST.getlist('labels'):
+		try:
+			new_labels.append(int(label_str))
+		except ValueError: pass
+
+	# Remove unused labels
+	labels_to_remove = [l for l in old_labels if l not in new_labels]
+	labels_to_add = [l for l in new_labels if l not in old_labels]
+
+	# * Old labels won't have integrity issues so eliminate try block
+	for label_id in labels_to_remove:
+		issue.labels.remove(Label.objects.get(id=label_id))
+		update(issue=issue, user=request.user, mode=IssueHistory.UNLABEL, content=label_id)
+
+	# Add new labels
+	for label_id in labels_to_add:
+		try:
+			issue.labels.add(Label.objects.get(id=label_id))
+			update(issue=issue, user=request.user, mode=IssueHistory.LABEL, content=label_id)
+		except Label.DoesNotExist:
+			pass
+
+	issue.save()
+	notify(issue, request.user, 'mail/issue_labeled.html', {'issue': issue, 'old_labels': old_labels, 'new_labels': new_labels})
+
+def comment(issue, request):
+	if not request.user.has_perm('issues.comment_issue'):
+		return	# Audit fail
+
+	content = request.POST.get('content')
+	if content:
+		update(issue=issue, user=request.user, content=content)
+		notify(issue, request.user, 'mail/issue_general.html', {'issue': issue, 'comment': content})
+
+def toggle_state(issue, request):
+	if not request.user.has_perm('issues.toggle_issue'):
+		return	# Audit fail
+
+	issue.is_open = not issue.is_open
+	update(issue=issue, user=request.user, mode=(IssueHistory.REOPEN if issue.is_open else IssueHistory.CLOSE))
+	notify(issue, request.user, 'mail/issue_general.html', {'issue': issue})
+
+def toggle_star(issue, request):
+	if issue.starring.filter(id=request.user.id).count():
+		issue.starring.remove(request.user)
+	else:
+		issue.starring.add(request.user)
+
+def edit(issue, request):
+	if request.user == issue.creator or request.user.has_perm('issues.change_issue'):
+		pass	# Audit success
+	else:
+		pass	# Audit fail
