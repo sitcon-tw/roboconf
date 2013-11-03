d94fa367549ac8eb301a8391392f145f82214676
Poren Chiang <ren.chiang@gmail.com>
2013-11-03 14:03:42 +0800

    Fix typo on len -> length, also add line break consideration

--- core/static/js/override.js
+++ core/static/js/override.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("textarea").keyup(function(e) {
 		var $t = $(this);
-		var lines = parseInt($t.val().len / 56);
+		var lines = parseInt($t.val().length / 56) + $t.val().match(/\n/g).length;
 		var em = parseFloat($t.css("font-size"));
 		var dest = parseInt((lines + 2) * em * 1.15) + 
 			parseFloat($t.css("border-top-width") || 0) + parseFloat($t.css("border-bottom-width") || 0);
