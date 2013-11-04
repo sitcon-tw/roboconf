f0326ae182f3aa5806950ffcd564d3cc6603d56e
Poren Chiang <ren.chiang@gmail.com>
2013-11-04 19:47:12 +0800

    Separated sorting to individual functions

--- users/utils.py
+++ users/utils.py
@@ -22,3 +22,11 @@ def validate_email(email):
 def generate_password():
 	# Generate a password with length 12
 	return urlsafe_b64encode(urandom(8))[:-1]
+
+GROUP_PRIORITY = [3, 1, 6, 5, 8, 2, 11]	# Sort by group type, team lead -> staff -> consultant
+def get_user_sorting_key(user):
+	groups = [g.id for g in user.groups.all()]
+	identity = ''.join([str(1 - groups.count(i)) for i in GROUP_PRIORITY])	# Sort by identity first
+	title = user.profile.title.ljust(5)
+	name = get_user_name(user)
+	return ''.join((identity, title, name))
--- users/views/list.py
+++ users/views/list.py
@@ -2,30 +2,27 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from users.models import *
+from users.utils import get_user_sorting_key
+
+def sorted_users(group_id=None):
+	users = User.objects.filter(is_active=True)
+	if group_id:
+		users = users.filter(groups__id=group_id) 
+	return sorted(users, key=get_user_sorting_key)
 
 @login_required
 def list(request):
-	dataset = User.objects
-	group = request.GET.get('g')
-	if group and str(group).isdigit():
-		dataset = dataset.filter(groups__pk=group)
-
+	group = request.GET.get('g', '')
 	return render(request, 'users_list.html', {
-		'users': dataset.order_by('username').filter(is_active=True).all(),
+		'users': sorted_users(group_id=(None if not group.isdigit() else group)),
 		'categories': GroupCategory.objects.all(),
-		'filter': filter,
+		'filter': group,
 	})
 
 @login_required
 def contacts(request):
 	dataset = User.objects.filter(is_active=True)
-	priority = [3, 1, 6, 5, 8, 2, 11]	# Sort by group type, team lead -> staff -> consultant
-	def key(user):
-		groups = [g.id for g in user.groups.all()]
-		identity = ''.join([str(1 - groups.count(i)) for i in priority])	# Sort by identity first
-		return '%s%s' % (identity, user.profile.title.ljust(5), user.profile.display_name)
-	users = sorted(dataset, key=key)
 	return render(request, 'users_contacts.html', {
-		'users': users,
+		'users': sorted_users(),
 		'show_details': request.user.groups.filter(id=11).exists(),	# Only show cellphone to staff
 	})
