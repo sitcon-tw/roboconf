d29e23a732ba56f0c038475e3a6d291fe0cb7576
Poren Chiang <ren.chiang@gmail.com>
2013-11-06 04:03:40 +0800

    Fix wrong use of timedelta

--- core/templatetags/dateutils.py
+++ core/templatetags/dateutils.py
@@ -8,7 +8,9 @@ def smartdate(date):
 	delta = now() - date
 	local = localtime(now())
 	localdate = localtime(date)
-	if delta.days < 1 and local.hour >= delta.hours:
+	if delta.seconds < 60:
+		return u'\u4e0d\u4e45\u524d'
+	if delta.days < 1 and localdate.day >= local.day:
 		return '%d:%d' % (localdate.hour, localdate.minute)
 	if local.year >= localdate.year:
 		return u'%d\u6708%d\u65e5' % (localtime.year, localdate.month)
