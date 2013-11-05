a8d7d328485f5060accb4e0c72afe709c65d416b
Poren Chiang <ren.chiang@gmail.com>
2013-11-06 04:06:37 +0800

    Fix date format

--- core/templatetags/dateutils.py
+++ core/templatetags/dateutils.py
@@ -11,7 +11,7 @@ def smartdate(date):
 	if delta.seconds < 60:
 		return u'\u4e0d\u4e45\u524d'
 	if delta.days < 1 and localdate.day >= local.day:
-		return '%d:%d' % (localdate.hour, localdate.minute)
+		return '%d:%2d' % (localdate.hour, localdate.minute)
 	if local.year >= localdate.year:
 		return u'%d\u6708%d\u65e5' % (localtime.year, localdate.month)
 	return localdate.strftime('%x')
