8678087edab6e78111ce0931976fb4f0e096e012
Poren Chiang <ren.chiang@gmail.com>
2013-11-06 16:04:38 +0800

    Add primitive docs edit functionality

--- docs/urls.py
+++ docs/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 		url(r'^$', views.main, name='main'),
 		url(r'^folder/(?P<nidb64>[0-9A-Za-z_\-]+)$', views.folder, name='folder'), 
 		url(r'^view/(?P<nidb64>[0-9A-Za-z_\-]+)$', views.view, name='view'), 
+		url(r'^edit/(?P<nidb64>[0-9A-Za-z_\-]+)$', views.edit, name='edit'), 
 	)
--- docs/views/__init__.py
+++ docs/views/__init__.py
@@ -1,2 +1,3 @@
 from folder import main, folder
 from view import view
+from edit import edit
