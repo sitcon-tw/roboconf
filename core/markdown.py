dadbf566200dead4620891396ef2289062cdfbe0
Poren Chiang <ren.chiang@gmail.com>
2013-11-06 17:14:45 +0800

    Fix misaka parser arguments

--- core/markdown.py
+++ core/markdown.py
@@ -5,11 +5,10 @@ BASE_EXTENSIONS = m.EXT_FENCED_CODE | m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.E
 def render_inline(text, autoescape=True):
 	return m.html(text, 
 		extensions=BASE_EXTENSIONS,
-		render_flags=(m.HTML_ESCAPE if autoescape else None) | m.HTML_HARD_WRAP,
+		render_flags=(m.HTML_ESCAPE if autoescape else 0) | m.HTML_HARD_WRAP,
 	)
 
 def render_document(text):
 	return m.html(text,
 		extensions=m.EXT_TABLES | BASE_EXTENSIONS,
-		render_flags=None,
 	)
