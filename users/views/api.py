29971693dcdd40e32506e129ae20d583464fd030
Poren Chiang <ren.chiang@gmail.com>
2013-11-08 02:28:08 +0800

    Add user listing API

--- users/views/api.py
+++ users/views/api.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from core.api import route
+from users.utils import get_avatar_url
 
 def get(name):
 	try:
@@ -11,7 +12,6 @@ def get(name):
 	if not user or not user.is_active:
 		return {'status': 'invalid'}
 
-	from users.utils import get_avatar_url
 	return {
 		'status': 'success',
 		'name': user.username,
@@ -19,9 +19,17 @@ def get(name):
 		'title': user.profile.title,
 	}
 
+def list():
+	result = {}
+	for u in User.objects.filter(is_active=True, group__id=11):
+		result[u.username] = {
+			"title": u.profile.title,
+			"avatar": get_avatar_url(user.email),
+		}
 
 USERS_API_PATTERN = (
 	('get', get, ['name']),
+	('list', list, []),
 )
 
 def api(request):
