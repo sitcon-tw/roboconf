a9b3b07f8656c023f197cea246e546c415a20649
Poren Chiang <ren.chiang@gmail.com>
2013-11-07 22:06:14 +0800

    Fixing variable sequence

--- users/views/api.py
+++ users/views/api.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from core.api import route
 
-USERS_API_PATTERN = (
-	('get', get, ['name']),
-)
-
-def api(request):
-	return route(request, USERS_API_PATTERN)
-
 def get(name):
 	try:
 		user = User.objects.get(username=name)
@@ -25,3 +18,11 @@ def get(name):
 		'avatar': get_avatar_url(user.email),
 		'title': user.profile.title,
 	}
+
+
+USERS_API_PATTERN = (
+	('get', get, ['name']),
+)
+
+def api(request):
+	return route(request, USERS_API_PATTERN)
