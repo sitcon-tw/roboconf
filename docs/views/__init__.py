004e1621b2dd64f5b29a6c1be66250fab561aee0
Poren Chiang <ren.chiang@gmail.com>
2013-11-11 02:24:33 +0800

    Created **SUPER DIRTY** docs creation view.

--- docs/views/__init__.py
+++ docs/views/__init__.py
@@ -1,3 +1,4 @@
 from folder import main, folder
+from create import create
 from view import view
 from edit import edit
---
+++ docs/views/create.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.core.exceptions import PermissionDenied
+from docs.models import File, Permission, BlobText
+from docs.perms import get_perms
+from docs.utils import parse_nid
+
+def create(request):
+	f = parse_nid(File, request.GET.get('folder'))
+	if not f: raise Http404
+
+	perms = get_perms(request.user, f)
+	if Permission.EDIT not in perms:
+		if request.user.is_authenticated():
+			raise PermissionDenied 	# Access forbidden
+		else:
+			return redirect(reverse('users:login'))
+
+	if request.method == 'POST':
+		name = request.POST.get('name')
+		text = request.POST.get('text')
+		is_markdown = request.POST.get('is_markdown')
+
+		if name:
+			t = BlobText(text=text)
+			if is_markdown:
+				t.format = BlobText.MARKDOWN
+			t.save()
+
+			r = Revision(user=request.user)
+			r.base_revision = f.current_revision
+			r.text = t
+			r.save()
+
+			fil = File(parent=f)
+			fil.name = name
+			fil.current_revision = r
+			f.save()
+
+			status = 'success'
+		else:
+			status = 'invalid_name'
+	else:
+		status = ''
+
+	return render(request, 'docs_create.html', {
+		'folder': f,
+		'status': status,
+	})
