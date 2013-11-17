afb98c0128180851fa5d2d4bfd20f57c5a3fc7cd
Poren Chiang <ren.chiang@gmail.com>
2013-11-18 02:57:55 +0800

    Migrate to Django 1.6

--- docs/utils.py
+++ docs/utils.py
@@ -1,29 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from docs.models import File, Folder
-
-# == snippet from Django 1.6 dev snapshot ==
-# ** remove when 1.6 offically came out **
-import base64
-
-def urlsafe_base64_encode(s):
-    """
-    Encodes a bytestring in base64 for use in URLs, stripping any trailing
-    equal signs.
-    """
-    return base64.urlsafe_b64encode(s).rstrip(b'\n=')
-
-def urlsafe_base64_decode(s):
-    """
-    Decodes a base64 encoded string, adding back any trailing equal signs that
-    might have been stripped.
-    """
-    s = s.encode('utf-8') # base64encode should only return ASCII.
-    try:
-        return base64.urlsafe_b64decode(s.ljust(len(s) + len(s) % 4, b'='))
-    except (LookupError, BinasciiError) as e:
-        raise ValueError(e)
-# == end snippet ==
 
 NID_NAMESPACES = {'F': File, 'D': Folder}
 
--- users/token.py
+++ users/token.py
@@ -1,29 +1,7 @@
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.models import User
-
-# == snippet from Django 1.6 dev snapshot ==
-# ** remove when 1.6 offically came out **
-import base64
-
-def urlsafe_base64_encode(s):
-    """
-    Encodes a bytestring in base64 for use in URLs, stripping any trailing
-    equal signs.
-    """
-    return base64.urlsafe_b64encode(s).rstrip(b'\n=')
-
-def urlsafe_base64_decode(s):
-    """
-    Decodes a base64 encoded string, adding back any trailing equal signs that
-    might have been stripped.
-    """
-    s = s.encode('utf-8') # base64encode should only return ASCII.
-    try:
-        return base64.urlsafe_b64decode(s.ljust(len(s) + len(s) % 4, b'='))
-    except (LookupError, BinasciiError) as e:
-        raise ValueError(e)
-# == end snippet ==
 
 def generate_uid(user):
     return urlsafe_base64_encode(force_bytes(user.pk))
