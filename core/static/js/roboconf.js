b666968ffbab486c937563508dc92fa85d501277
Poren Chiang <ren.chiang@gmail.com>
2013-12-08 12:25:39 +0800

    Extract ajax calls to separate project lib

---
+++ core/static/js/roboconf.js
@@ -0,0 +1,25 @@
+$(document).bind('ajaxSend', function(e, xhr, settings) {
+	if (!(/^(GET|HEAD|OPTIONS|TRACE)$/.test(settings.type)))
+		xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
+});
+
+var Roboconf = (function(){
+	function defSuccess(d,t,j) { window.location.reload(); }
+	return {
+		post: function(url, args, options) {
+			$.ajax({
+				data: args,
+				success: (options && options.success) || defSuccess,
+				url: url, type: 'POST'
+			});
+		},
+		put: function(url, args, options) {
+			$.ajax({
+				contentType: 'application/json',
+				data: JSON.stringify(args),
+				success: (options && options.success) || defSuccess,
+				url: url, type: 'PUT'
+			});
+		}
+	}
+})()
