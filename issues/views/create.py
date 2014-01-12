50363cdb3ee037816f5665f2a6f2f293f974f690
Poren Chiang <ren.chiang@gmail.com>
2014-01-13 01:02:25 +0800

    Fix missing import x 2

--- issues/views/create.py
+++ issues/views/create.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils import dateparse
