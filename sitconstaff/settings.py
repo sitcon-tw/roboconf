e2299571df3f4bc3cf3131b035a73ac5fd18076a
Poren Chiang <ren.chiang@gmail.com>
2014-01-16 16:00:13 +0800

    Add .travis.yml

--- sitconstaff/settings.py
+++ sitconstaff/settings.py
@@ -17,8 +17,8 @@ DATABASES = {
         'NAME': 'sitcon_staff_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': 'local',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
