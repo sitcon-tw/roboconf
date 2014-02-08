f100c5fe2e8bfb256a60e061799bd8cee216fcab
Poren Chiang <ren.chiang@gmail.com>
2014-02-08 17:52:12 +0800

    Fix #7 on profile

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from users.utils import get_avatar_url
 
 def profile(request, username):
 	user = get_object_or_404(User, username=username)
