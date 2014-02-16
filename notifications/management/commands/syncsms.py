e600f8356f2f97eba552894d1c36751a66c888c3
Poren Chiang <ren.chiang@gmail.com>
2014-02-17 01:09:59 +0800

    Alter SmsMessage class to be more generic

--- notifications/management/commands/syncsms.py
+++ notifications/management/commands/syncsms.py
@@ -1,23 +1,30 @@
 from django.core.management.base import NoArgsCommand
+from django.conf import settings
 from django.utils.html import strip_tags
 from notifications.sms import SmsMessage
-
 from notifications.models import *
 
 class Command(NoArgsCommand):
-	help = "Checks and sends messages from notification queue."
+	help = "Checks and sends SMS from notification queue."
 
 	def handle_noargs(self, **options):
 		messages = Message.objects.filter(method=Message.SMS, is_sent=False)
 		if messages.count():
 			for message in messages:
 				sms = SmsMessage()
-				sms.to = message.receiver
-				sms.text = strip_tags(message.content)
 
 				if message.sender:
 					sms.from_sender = message.sender
+				else:
+					sms.from_sender = settings.DEFAULT_SMS_SENDER
 
-				item.is_sent = sms.send()
-				item.save()
+				receiver = message.receiver
+				if receiver.startswith('0') and not receiver.startswith('00'):
+					sms.to = settings.DEFAULT_SMS_COUNTRY_CODE + receiver[1:]
+				else:
+					sms.to = receiver
+	
+				sms.text = strip_tags(message.content)
 
+				message.is_sent = sms.send()
+				message.save()
