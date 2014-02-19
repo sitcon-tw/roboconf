977094c5ff96cbed913ed1b3e9eb01d8f63c7cb1
Poren Chiang <ren.chiang@gmail.com>
2014-02-19 20:34:19 +0800

    Fix #9 missing import

--- users/views/edit.py
+++ users/views/edit.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from users.models import *
 
 @login_required
