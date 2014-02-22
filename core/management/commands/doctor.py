390104aa01996773303c94957dfb58c34bc4eb78
Poren Chiang <ren.chiang@gmail.com>
2014-02-22 12:24:58 +0800

    Add pingdom-like doctor service

---
+++ core/management/commands/doctor.py
@@ -0,0 +1,14 @@
+from django.core.management.base import NoArgsCommand
+from django.conf import settings
+from urllib2 import urlopen, URLError
+
+class Command(NoArgsCommand):
+	help = "Monitor site activities periodically."
+
+	def handle_noargs(self, **options):
+		for host in settings.ALLOWED_HOSTS:
+			try:
+				urlopen('http://%s/' % host)
+			except URLError:
+				# TODO: Send notifications to admin
+				pass
