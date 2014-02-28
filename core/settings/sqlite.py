c696efaee712ba374867d90b9d95633e4ff7cd6b
Poren Chiang <ren.chiang@gmail.com>
2014-02-28 18:42:51 +0800

    Add SQLite as target

---
+++ core/settings/sqlite.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'roboconf.db'), 
+    }
+}
