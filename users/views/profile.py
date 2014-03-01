310f0b3a77638694d9682cc864a078e4c54cf20f
Poren Chiang <ren.chiang@gmail.com>
2014-03-01 22:09:14 +0800

    Fix# Add missing import

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.views import redirect_to_login
