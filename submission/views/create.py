379cd592fc6f3f0f5f2ded765a4b58a2e21f02c1
Pellaeon Lin <nfsmwlin@gmail.com>
2014-08-16 17:07:05 +0800

    Embed submission rule doc

--- submission/views/create.py
+++ submission/views/create.py
@@ -2,28 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from submission.models import Submission
+from core.formatting import render_document
+from docs.node import Node
+from django.conf import settings
 
 @login_required
 def create(request):
 	context = {
 			'user': request.user,
+			'rule': render_document(Node(nid=settings.SUBMISSION_RULE_DOCID).model.current_revision.text.text)
 			}
 
 	if request.POST.get('submit'):
-		receiver_target = request.POST.get('receiver', '').split(',')
-		if 'staff' in receiver_target:
-			for user in User.objects.exclude(email='', is_active=False):
-				receivers[user.email] = user.profile.name()
-
-		if request.POST.get('receivers'):
-			additional_receivers = request.POST.get('receivers').strip().split('\n')
-			for entry in additional_receivers:
-				if ':' in entry:
-					name, addr = parse_address(entry)
-					receivers[addr] = name
-				else:
-					receivers[entry] = ''
-
-		messages = []
+		sub = Submission()
+		sub.user = request.user
 
 	return render(request, 'submission/create.html', context)
