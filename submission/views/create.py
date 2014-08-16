9bbc3b9c5bfaf6753e4ecc5d0611ad915c572bb4
Pellaeon Lin <nfsmwlin@gmail.com>
2014-08-16 15:55:37 +0800

    Basic template for submission

--- submission/views/create.py
+++ submission/views/create.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import permission_required
-from notifications.models import Message
-from notifications.utils import *
+from django.contrib.auth.decorators import login_required
+from submission.models import Submission
 
-@permission_required('notifications.add_message')
+@login_required
 def create(request):
-	context = {}
+	context = {
+			'user': request.user,
+			}
 
 	if request.POST.get('submit'):
-
-		if request.POST.get('method') == 'sms':
-			pass
-
-		sender = ''
-		receivers = {}
-
-		if request.POST.get('sender_email'):
-			sender = format_address(request.POST.get('sender_name'), request.POST.get('sender_email'))
-		
 		receiver_target = request.POST.get('receiver', '').split(',')
 		if 'staff' in receiver_target:
 			for user in User.objects.exclude(email='', is_active=False):
@@ -35,21 +26,4 @@ def create(request):
 
 		messages = []
 
-		for email, name in receivers.iteritems():
-			message = send_template_mail(
-				sender, 
-				format_address(name, email), 
-				'mail/notification_general.html', 
-				{
-					'subject': request.POST.get('subject'),
-					'content': request.POST.get('content'),
-					'reply_to': request.POST.get('reply_address'),
-				},
-				autosave=False,
-			)
-			messages.append(message)
-
-		Message.objects.bulk_create(messages)
-		context['status'] = 'success'
-
-	return render(request, 'notifications/create.html', context)
+	return render(request, 'submission/create.html', context)
