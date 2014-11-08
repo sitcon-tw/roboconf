6b560b313f5321c1b33ee4643d6debed4070b903
dv <wdv4758h@gmail.com>
2014-11-08 23:25:16 +0800

    [Add] delete for submission

--- submission/urls.py
+++ submission/urls.py
@@ -4,4 +4,5 @@ from submission import views
 urlpatterns = patterns('',
 		url(r'^$', views.list, name='list'),
 		url(r'^new$', views.create, name='create'),
+		url(r'^delete$', views.delete, name='delete'),
 	)
--- submission/views/__init__.py
+++ submission/views/__init__.py
@@ -1,2 +1,3 @@
 from list import list
 from create import create
+from delete import delete
