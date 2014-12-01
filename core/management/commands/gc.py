0b89cdf94dedf2e50a93455c98de3722e49872e2
Poren Chiang <ren.chiang@gmail.com>
2014-12-01 21:37:42 +0800

    Since we've moved to NCTU, no need to do GC now

--- core/management/commands/gc.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import NoArgsCommand
-import notifications.gc as notifications
-
-class Command(NoArgsCommand):
-	help = "Cleans up unused database entries."
-
-	def handle_noargs(self, **options):
-		print notifications.clean()
