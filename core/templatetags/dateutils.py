4e1f5a691d9a83b2bd35615d4db3e4e275f768a5
Poren Chiang <ren.chiang@gmail.com>
2014-12-02 02:07:25 +0800

    Include expiration time in notification

--- core/templatetags/dateutils.py
+++ core/templatetags/dateutils.py
@@ -12,6 +12,6 @@ def smartdate(date):
 		return u'\u4e0d\u4e45\u524d'
 	if delta.days < 1 and localdate.day >= local.day:
 		return '%d:%02d' % (localdate.hour, localdate.minute)
-	if local.year >= localdate.year:
+	if localdate.year >= local.year:
 		return u'%d\u6708%d\u65e5' % (localdate.month, localdate.day)
 	return localdate.strftime('%x')
