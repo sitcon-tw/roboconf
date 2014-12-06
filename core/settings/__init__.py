d328760bca1fadac511db0f5c079b2a80394435d
dv <wdv4758h@gmail.com>
2014-12-06 18:14:39 +0800

    move environ variable to a json file

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
+import json
+
+SETTINGS_DIR = os.path.dirname(__file__)
+
+with open(SETTINGS_DIR + '/settings.json', 'r') as f:
+    os.environ.update(json.load(f))
 
 if 'HEROKU' in os.environ:
 	from .heroku import *
