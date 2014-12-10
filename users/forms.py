e92b852c1225b73a9c76959c3196615c1efb435d
dv <wdv4758h@gmail.com>
2014-12-11 03:05:02 +0800

    Move photo and departure field to UserProfile

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,16 @@ from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import PasswordResetForm as DjangoPasswordResetForm
 from django.contrib.auth.models import User
+from django import forms
 from notifications.utils import format_address, send_template_mail
 from users.token import generate_uid, generate_token
+from users.models import UserProfile
+
+class CommiterProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		fields = '__all__'
+		exclude = ('user', 'title')
 
 class PasswordResetForm(DjangoPasswordResetForm):
 	def clean_email(self):
