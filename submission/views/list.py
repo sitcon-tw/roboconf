8dab430d5bf56c2860d7a6bf57ad53de568f83b6
Pellaeon Lin <nfsmwlin@gmail.com>
2015-01-28 14:14:05 +0800

    order_by doesn't work, use template filter dictsort

--- submission/views/list.py
+++ submission/views/list.py
@@ -8,14 +8,14 @@ import datetime
 def list(request):
     if request.user.has_perm('submission.review'):
         context = {
-            'submissions': Submission.objects.order_by('type'),
+            'submissions': Submission.objects.all(),
             'user': request.user,
             'submission_end': SUBMISSION_END,
             'expired': (SUBMISSION_END < datetime.datetime.now() ),
         }
     else:
         context = {
-            'submissions': request.user.submissions.order_by('type'),
+            'submissions': request.user.submissions.all(),
             'user': request.user,
             'submission_end': SUBMISSION_END,
             'expired': (SUBMISSION_END < datetime.datetime.now() ),
