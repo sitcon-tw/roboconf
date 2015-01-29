557b0e191c4720d4138112b4965ee664b4abdaf1
dv <wdv4758h@gmail.com>
2015-01-29 14:47:00 +0800

    Update# add submission:score default value support

--- submission/views/score.py
+++ submission/views/score.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from submission.models import Submission
+from submission.models import Submission, Score
 from submission.forms import ScoreForm
 from core.settings.base import SUBMISSION_END
 import json
 import datetime
 
+def get_score(submission_id, user_id):
+    result = Score.objects.filter(submission_id=submission_id, user_id=user_id)
+
+    if len(result) > 0:
+        result = result[0]
+    else:
+        result = None
+
+    return result
+
 @login_required
 def score(request):
     if request.user.has_perm('submission.review'):
+
+        submissions = Submission.objects.order_by('type', 'id')
+        scores = [ get_score(s.id, request.user.id) for s in submissions ]
+
         context = {
-            'submissions': Submission.objects.all(),
+            'submissions': zip(submissions, scores),
             'user': request.user,
             'submission_end': SUBMISSION_END,
             'submission_review': True,
