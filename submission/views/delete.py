7b83cd066dc67d9068ee6eb9a247cd52f5ed0fc4
Pellaeon Lin <nfsmwlin@gmail.com>
2015-01-31 13:57:09 +0800

    Fix# Do not allow edit after SUBMISSION_END

--- submission/views/delete.py
+++ submission/views/delete.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.http import Http404
 from submission.models import Submission
+from core.settings.base import SUBMISSION_END
+import datetime
 
 @login_required
 def delete(request):
+    if SUBMISSION_END < datetime.datetime.now():
+        raise Http404
     submission = Submission.objects.get(id=request.POST['submission_id'])
 
     if request.user == submission.user:
