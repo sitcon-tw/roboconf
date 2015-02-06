38cdc920bd96814f979eb8c1cba253ac6eb6a16e
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-06 21:02:47 +0800

    Update# REST API for UserProfile

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import *
+from users.models import UserProfile
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
+
+class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = UserProfile.objects.filter(user__is_active=True)
+    serializer_class = UserProfileSerializer
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
