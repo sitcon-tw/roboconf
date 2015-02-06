3c0dab7bef161b72acda054144e63f2a8b36c2ef
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-06 21:02:47 +0800

    Update# REST API for UserProfile

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from users.models import UserProfile
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
-        #extra_kwargs = {
-        #        'url': {'view_name': 'user', 'lookup_field': 'username'}
-        #        }
+        fields = ('url', 'profile', 'username', 'groups')
 
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('url', 'display_name', 'bio', 'title', 'avatar')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'userprofiles', views.UserProfileViewSet)
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
