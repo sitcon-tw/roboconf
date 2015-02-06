fe2f76f2afe66a45d92090b3456d786aa707f7bc
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-06 18:33:10 +0800

    Update# basic api for User and Group

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
+        #extra_kwargs = {
+        #        'url': {'view_name': 'user', 'lookup_field': 'username'}
+        #        }
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
