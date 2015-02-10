f74e4aa300679a1fb0759b9210ffb214516571b0
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-11 00:06:49 +0800

    Update# add admin actions for activity for quickly building entire schedule

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from schedule.models import *
+from schedule.models import Room
+import copy
 
-admin.site.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    actions = ['copy_activity', 'propagate_all_rooms']
+
+    def copy_activity(self, request, queryset):
+        for a in queryset:
+            a_copy = copy.copy(a)
+            a_copy.pk = None
+            a_copy.save()
+
+    def propagate_all_rooms(self, request, queryset):
+        for a in queryset:
+            for r in Room.objects.all():
+                if not a.room == r:
+                    a_copy = copy.copy(a)
+                    a_copy.pk = None
+                    a_copy.room = r
+                    a_copy.save()
+
+admin.site.register(Activity, ActivityAdmin)
 admin.site.register(Timeslot)
 admin.site.register(Room)
