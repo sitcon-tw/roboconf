99314e8c24a6bd725fe7c1446da145fa4536d0e7
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-10 21:22:50 +0800

    Update# bulk submission actions for admin site

--- submission/admin.py
+++ submission/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from submission.models import *
 
-admin.site.register(Submission)
 admin.site.register(Score)
+
+class SubmissionAdmin(admin.ModelAdmin):
+    actions = ['make_editing', 'make_accepted', 'make_rejected', 'make_reviewing', 'make_pending']
+
+    def make_editing(self, request, queryset):
+        queryset.update(status='E')
+
+    def make_accepted(self, request, queryset):
+        queryset.update(status='A')
+
+    def make_rejected(self, request, queryset):
+        queryset.update(status='R')
+
+    def make_reviewing(self, request, queryset):
+        queryset.update(status='V')
+
+    def make_pending(self, request, queryset):
+        queryset.update(status='P')
+
+admin.site.register(Submission, SubmissionAdmin)
