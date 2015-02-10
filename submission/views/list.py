65cae531b8ea7184620e0aa3cc693ac711d7ca17
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-10 20:02:16 +0800

    Update# show count of users who has submission

--- submission/views/list.py
+++ submission/views/list.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from submission.models import Submission
 from core.settings.base import SUBMISSION_END
+from users.models import UserProfile
 import datetime
 
 @login_required
 def list(request):
     if request.user.has_perm('submission.review'):
+        profilelist=[]
+        for p in UserProfile.objects.all():
+            if p.has_submission():
+                profilelist.append(p)
+        
         context = {
             'submissions': Submission.objects.order_by('type', 'id'),
             'user': request.user,
             'submission_end': SUBMISSION_END,
             'submission_review': True,
             'expired': (SUBMISSION_END < datetime.datetime.now() ),
+            'submitter_count': len(profilelist),
         }
     else:
         context = {
