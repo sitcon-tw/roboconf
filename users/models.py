e8460c12b282ec31ee2d682390e910713bbe6496
Pellaeon Lin <nfsmwlin@gmail.com>
2015-02-10 20:02:16 +0800

    Update# show count of users who has submission

--- users/models.py
+++ users/models.py
@@ -45,6 +45,9 @@ class UserProfile(models.Model):
 	def is_trusted(self):
 		return self.is_authorized() and self.user.has_perm('auth.change_user')
 
+	def has_submission(self):
+		return True if self.user.submissions.count() > 0 else False
+
 
 class GroupCategory(models.Model):
 	name = models.CharField(max_length=30)
