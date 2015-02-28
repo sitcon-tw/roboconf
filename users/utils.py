df4c9dc58a7172751ad53bd12e05b561248ce77c
Poren Chiang <ren.chiang@gmail.com>
2015-02-28 11:13:23 +0800

    Abstracting group sorting mechanism

--- users/utils.py
+++ users/utils.py
@@ -7,7 +7,7 @@ def generate_password():
 	# Generate a password with length 12
 	return urlsafe_b64encode(urandom(8))[:-1]
 
-GROUP_PRIORITY = [3, 1, 6, 5, 7, 8, 4, 9, 2, 14, 15, 13, 12, 11, 10]	# Sort by team lead -> staff -> consultant
+GROUP_PRIORITY = [3, 1, 6, 7, 5, 8, 4, 9, 2, 14, 19, 20, 15, 13, 18, 12, 11, 10]	# Sort by team lead -> staff -> consultant
 def get_user_sorting_key(user):
 	groups = [g.id for g in user.groups.all()]
 	identity = ''.join([str(1 - groups.count(i)) for i in GROUP_PRIORITY])	# Sort by identity first
@@ -18,11 +18,14 @@ def get_user_sorting_key(user):
 def sorted_users(users):
 	return sorted(users, key=get_user_sorting_key)
 
-def get_group_sorting_key(category):
+def sorted_groups(groups):
+	return sorted(groups, key=get_group_sorting_key)
+
+def get_group_sorting_key(group):
 	try:
-		return GROUP_PRIORITY.index(category.id)
+		return GROUP_PRIORITY.index(group.id)
 	except ValueError:
 		return len(GROUP_PRIORITY)
 
 def sorted_categories():
-	return { category : sorted(category.groups.all(), key=get_group_sorting_key) for category in GroupCategory.objects.all() }
+	return { category : sorted_groups(category.groups.all()) for category in GroupCategory.objects.all() }
