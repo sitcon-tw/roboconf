2033fd7b574c01dbe6f50d8ce3456b19034bbff5
Poren Chiang <ren.chiang@gmail.com>
2015-05-03 23:11:50 +0800

    Fix photo upload path

--- users/views/edit.py
+++ users/views/edit.py
@@ -8,94 +8,94 @@ from users.utils import *
 
 @login_required
 def edit(request, username):
-	user = get_object_or_404(User, username=username)
-	privileged = request.user.has_perm('auth.change_user')
+    user = get_object_or_404(User, username=username)
+    privileged = request.user.has_perm('auth.change_user')
 
-	if not (user == request.user or privileged):
-		from django.core.exceptions import PermissionDenied
-		raise PermissionDenied
+    if not (user == request.user or privileged):
+        from django.core.exceptions import PermissionDenied
+        raise PermissionDenied
 
-	errors = []
-	status = ''
+    errors = []
+    status = ''
 
-	action = request.POST.get('action')
-	if action and privileged:
-		if action == 'activate':
-			user.is_active = True
-		elif action == 'deactivate':
-			user.is_active = False
-		user.save()
-		status = 'success'
+    action = request.POST.get('action')
+    if action and privileged:
+        if action == 'activate':
+            user.is_active = True
+        elif action == 'deactivate':
+            user.is_active = False
+        user.save()
+        status = 'success'
 
-	if request.POST.get('submit'):
-		profile = None
-		try:
-			profile = user.profile
-		except UserProfile.DoesNotExist:
-			profile = UserProfile(user=user)
+    if request.POST.get('submit'):
+        profile = None
+        try:
+            profile = user.profile
+        except UserProfile.DoesNotExist:
+            profile = UserProfile(user=user)
 
-		if privileged:
-			username = request.POST.get('username')
-			if username != user.username:
-				if username:
-					if User.objects.filter(username=username).count() < 1:
-						user.username = username
-					else:
-						errors += ['username', 'username_already_taken']
-				else:
-					errors += ['username', 'invalid_username']
+        if privileged:
+            username = request.POST.get('username')
+            if username != user.username:
+                if username:
+                    if User.objects.filter(username=username).count() < 1:
+                        user.username = username
+                    else:
+                        errors += ['username', 'username_already_taken']
+                else:
+                    errors += ['username', 'invalid_username']
 
-			groups = request.POST.getlist('groups')
-			old_groups = user.groups.all()
-			for group in old_groups:
-				if group.id not in groups:
-					user.groups.remove(group)
+            groups = request.POST.getlist('groups')
+            old_groups = user.groups.all()
+            for group in old_groups:
+                if group.id not in groups:
+                    user.groups.remove(group)
 
-			for group_id in groups:
-				try:
-					if group_id not in old_groups:
-						user.groups.add(Group.objects.get(id=group_id))
-				except Group.DoesNotExist: pass
+            for group_id in groups:
+                try:
+                    if group_id not in old_groups:
+                        user.groups.add(Group.objects.get(id=group_id))
+                except Group.DoesNotExist: pass
 
-			profile.title = request.POST.get('title')
+            profile.title = request.POST.get('title')
 
-		user.first_name = request.POST.get('first_name')
-		user.last_name = request.POST.get('last_name')
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
 
-		email = request.POST.get('email', '')
-		if email != user.email:
-			try:
-				validate_email(email)
+        email = request.POST.get('email', '')
+        if email != user.email:
+            try:
+                validate_email(email)
 
-				if User.objects.filter(email=email).count() < 1:
-					user.email = email
-				else:
-					errors += ['email', 'email_already_taken']
-			except ValidationError:
-				errors += ['email', 'invalid_email']
+                if User.objects.filter(email=email).count() < 1:
+                    user.email = email
+                else:
+                    errors += ['email', 'email_already_taken']
+            except ValidationError:
+                errors += ['email', 'invalid_email']
 
-		profile.display_name = request.POST.get('display_name')
-		profile.school = request.POST.get('school')
-		profile.bio = request.POST.get('bio')
-		profile.grade = request.POST.get('grade')
-		profile.phone = request.POST.get('phone')
+        profile.display_name = request.POST.get('display_name')
+        profile.school = request.POST.get('school')
+        profile.bio = request.POST.get('bio')
+        profile.grade = request.POST.get('grade')
+        profile.phone = request.POST.get('phone')
 
-                if request.FILES.get('photo'):
-                    profile.photo = request.FILES.get('photo')
+        if request.FILES.get('photo'):
+            profile.photo = request.FILES.get('photo')
 
-		profile.departure = request.POST.get('departure')
-		profile.comment = request.POST.get('comment')
+        profile.departure = request.POST.get('departure')
+        profile.comment = request.POST.get('comment')
 
-		if len(errors) < 1:
-			user.save()
-			profile.save()
-			status = 'success'
-		else:
-			status = 'error'
+        if len(errors) < 1:
+            user.save()
+            profile.save()
+            status = 'success'
+        else:
+            status = 'error'
 
-	return render(request, 'users/edit_profile.html', {
-		'u': user,
-		'categories': sorted_categories if privileged else None,
-		'errors': errors,
-		'status': status,
-	})
+    return render(request, 'users/edit_profile.html', {
+        'u': user,
+        'categories': sorted_categories if privileged else None,
+        'errors': errors,
+        'status': status,
+    })
