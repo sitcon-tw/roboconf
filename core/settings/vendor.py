78ab3b1fb49796a27536809677527ddaec437a97
Poren Chiang <ren.chiang@gmail.com>
2015-05-03 21:24:13 +0800

    Remove remaining submitter component

--- core/settings/vendor.py
+++ core/settings/vendor.py
@@ -11,13 +11,8 @@ DEFAULT_NOTIFICATION_SENDER = u'SITCON 夏令營行政系統:notifications@staff
 DEFAULT_ACCOUNTS_SENDER =  u'SITCON 夏令營行政系統:accounts@staff.sitcon.camp'
 DEFAULT_ISSUE_SENDER = u'SITCON 夏令營行政系統:issues@staff.sitcon.camp'
 
-SUBMITTER_ACCOUNTS_SENDER = u'SITCON:accounts@staff.sitcon.camp'
 USER_ISSUE_SENDER = u'{0} (SITCON):issues@staff.sitcon.camp'
 
 DEFAULT_SMS_SENDER = 'SITCON'
-
-SUBMISSION_START = datetime.datetime(2014, 12, 5, 12, 0, 0)
-SUBMISSION_END = datetime.datetime(2015, 1, 31, 6, 38, 0)
-SUBMISSION_RULE_DOCID = 'MUY'
 
 ISSUE_EXPIRE_TIMEDELTA = datetime.timedelta(hours=12)
