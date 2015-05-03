05fe2a7dd1476d169c92f00a7d114b64741c37d6
Poren Chiang <ren.chiang@gmail.com>
2015-05-03 13:51:41 +0800

    Remove JSON initialization

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -4,9 +4,6 @@ import json
 
 SETTINGS_DIR = os.path.dirname(__file__)
 
-with open(SETTINGS_DIR + '/settings.json', 'r') as f:
-    os.environ.update(json.load(f))
-
 if 'HEROKU' in os.environ:
 	from .heroku import *
 elif 'DEBUG' in os.environ:
