69d8abb25ceca47dc209a2f008afe8d3336a5e0d
Poren Chiang <ren.chiang@gmail.com>
2015-05-04 20:15:42 +0800

    Add experimental image cropping and resizing

---
+++ core/imaging.py
@@ -0,0 +1,17 @@
+from wand.image import Image
+
+def resize_image(image_file, size):
+    # Throws ValueError on bad image, according to Wand.py docs
+    with Image(file=image_file) as image:
+        # Crop to square image first
+        if image.width > image.height:
+            x = (image.width - image.height) / 2
+            image.crop(left=x, width=image.height)
+        elif image.height > image.width:
+            y = (image.height - image.width) / 2
+            image.crop(top=y, height=image.width)
+
+        if image.width > size:
+            image.resize(width=size, height=size)
+
+        return image.make_blob()
