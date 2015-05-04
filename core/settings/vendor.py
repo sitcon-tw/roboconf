9974224216cf9bd23472a1c614493e8219d04357
Poren Chiang <ren.chiang@gmail.com>
2015-05-04 12:06:37 +0800

    Found out that dropdown won’t be selected because byte string (arrgh)

--- core/settings/vendor.py
+++ core/settings/vendor.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import datetime
 
 ALLOWED_HOSTS = ['staff.sitcon.camp']
 SITE_URL = 'http://staff.sitcon.camp'
 
-DEFAULT_FROM_EMAIL = u'"SITCON 夏令營行政系統" <admin@staff.sitcon.camp>'
+DEFAULT_FROM_EMAIL = '"SITCON 夏令營行政系統" <admin@staff.sitcon.camp>'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
-DEFAULT_NOTIFICATION_SENDER = u'SITCON 夏令營行政系統:notifications@staff.sitcon.camp'
-DEFAULT_ACCOUNTS_SENDER =  u'SITCON 夏令營行政系統:accounts@staff.sitcon.camp'
-DEFAULT_ISSUE_SENDER = u'SITCON 夏令營行政系統:issues@staff.sitcon.camp'
+DEFAULT_NOTIFICATION_SENDER = 'SITCON 夏令營行政系統:notifications@staff.sitcon.camp'
+DEFAULT_ACCOUNTS_SENDER =  'SITCON 夏令營行政系統:accounts@staff.sitcon.camp'
+DEFAULT_ISSUE_SENDER = 'SITCON 夏令營行政系統:issues@staff.sitcon.camp'
 
-USER_ISSUE_SENDER = u'{0} (SITCON):issues@staff.sitcon.camp'
+USER_ISSUE_SENDER = '{0} (SITCON):issues@staff.sitcon.camp'
 DEFAULT_SMS_SENDER = 'SITCON'
 
 ISSUE_EXPIRE_TIMEDELTA = datetime.timedelta(hours=12)
