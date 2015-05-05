814da5ed2a188a7fda63337aadc885caa5d54bf3
Poren Chiang <ren.chiang@gmail.com>
2015-05-05 16:39:11 +0800

    Add the ability to mention groups
    Conflicts:
    	core/settings/base.py

--- issues/utils.py
+++ issues/utils.py
@@ -2,7 +2,7 @@ import re
 from django.conf import settings
 from django.db.models import Q
 from notifications.utils import format_address, send_template_mail, send_template_sms
-from users.models import User
+from users.models import User, Group
 
 def send_mail(sender, receiver, template_name, context):
 	context['sender'] = sender
@@ -20,11 +20,16 @@ def filter_mentions(content):
 	mention_tokens = set(re.findall(u'(?<=@)[0-9A-Za-z\u3400-\u9fff\uf900-\ufaff_\\-]+', content))
 	mentions = []
 	for mention in mention_tokens:
+		try:
 		mentionee = User.objects.filter(Q(username__istartswith=mention) | Q(profile__display_name__iexact=mention)).first()
 		if mentionee:
 			mentions.append(mentionee)
-	return mentions
+		else:
+			mention_group = Group.objects.filter(name__istartswith=mention).first()
+			if mention_group:
+				mentions.extend(User.objects.filter(group=mention_group))
+	return set(mentions)
 
 def is_issue_urgent(issue):
 	# Label 1 stands for urgent in current staff system
-	return issue.labels.filter(id=1).exists()
+	return issue.labels.filter(id=settings.URGENT_ISSUE_ID).exists()
