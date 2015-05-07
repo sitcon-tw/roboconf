955e8156afd70b905ad88818b3ce02abb65a8085
Poren Chiang <ren.chiang@gmail.com>
2015-05-07 15:08:21 +0800

    Fix #49, also integrate mentions to users.mentions module

--- issues/utils.py
+++ issues/utils.py
@@ -1,10 +1,7 @@
 import datetime
-import re
 from django.conf import settings
-from django.db.models import Q
 from django.utils import dateparse
 from notifications.utils import format_address, send_template_mail, send_template_sms
-from users.models import User, Group
 
 def send_mail(sender, receiver, template_name, context):
 	context['sender'] = sender
@@ -18,21 +15,6 @@ def send_sms(sender, receiver, template_name, context):
 	context['sender'] = sender
 	context['receiver'] = receiver
 	return send_template_sms('', receiver.profile.phone, template_name, context)
-
-def filter_mentions(content):
-	mention_tokens = set(re.findall(u'(?<=@)[0-9A-Za-z\u3400-\u9fff\uf900-\ufaff_\\-]+', content))
-	mentions, extra_receivers = [], []
-	for mention in mention_tokens:
-		mentionee = User.objects.filter(Q(username__istartswith=mention) | Q(profile__display_name__iexact=mention)).first()
-		if mentionee:
-			mentions.append(mentionee)
-		else:
-			mention_group = Group.objects.filter(name__istartswith=mention).first()
-			if mention_group:
-				mentions.extend(User.objects.filter(groups=mention_group))
-	if settings.BROADCAST_MAGIC_TOKEN in mention_tokens:
-		extra_receivers.append(settings.BROADCAST_EMAIL)
-	return set(mentions), extra_receivers
 
 def parse_date(value):
 	if len(value) <= 10:
