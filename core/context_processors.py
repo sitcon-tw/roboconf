d58aef6506648ea9bad4a52c74caddfc698af026
Poren Chiang <ren.chiang@gmail.com>
2015-05-07 14:57:49 +0800

    Move site context to dict object
    Conflicts:
    	core/templates/core/base.html
    	users/templates/mail/user_welcome.html

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,7 +2,10 @@ from django.conf import settings
 
 def site_url(request):
     return {
-        'site_url': settings.SITE_URL,
-        'site_name': settings.SITE_NAME,
-        'site_title': settings.SITE_TITLE,
+        'site': {
+            'url': settings.SITE_URL,
+            'name': settings.SITE_NAME,
+            'title': settings.SITE_TITLE,
+            'email': settings.BROADCAST_EMAIL,
+        }
     }
