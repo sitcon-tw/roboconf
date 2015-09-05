96bed5ffc6a129a19486cb9c2bcf9016d6269bd3
Philip Chou <pcchou@pcchou.me>
2015-09-05 11:55:09 +0800

    fix settings.json not found error

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -4,12 +4,13 @@ import json
 
 SETTINGS_DIR = os.path.dirname(__file__)
 
-with open(SETTINGS_DIR + '/settings.json', 'r') as f:
-    os.environ.update(json.load(f))
+if os.path.exists(os.path.join(SETTINGS_DIR, '/settings.json')):
+    with open(os.path.join(SETTINGS_DIR, '/settings.json'), 'r') as f:
+        os.environ.update(json.load(f))
 
 if 'HEROKU' in os.environ:
-	from .heroku import *
+    from .heroku import *
 elif 'DEBUG' in os.environ:
-	from .sqlite import *
+    from .sqlite import *
 else:
-	from .postgres import *
+    from .postgres import *
