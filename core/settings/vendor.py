e878f7d6e1cf919c5c4f292e2a233da93ee3b5d4
Poren Chiang <ren.chiang@gmail.com>
2015-09-09 21:09:53 +0800

    Found out that dropdown won’t be selected because byte string (arrgh)

--- core/settings/vendor.py
+++ core/settings/vendor.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import datetime
 
 ALLOWED_HOSTS = ['staff.sitcon.org']
 SITE_URL = 'https://staff.sitcon.org'
 
-DEFAULT_FROM_EMAIL = u'"SITCON 行政系統" <admin@staff.sitcon.org>'
+DEFAULT_FROM_EMAIL = '"SITCON 行政系統" <admin@staff.sitcon.org>'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
-DEFAULT_NOTIFICATION_SENDER = u'SITCON 行政系統:notifications@staff.sitcon.org'
-DEFAULT_ACCOUNTS_SENDER =  u'SITCON 行政系統:accounts@staff.sitcon.org'
-DEFAULT_ISSUE_SENDER = u'SITCON 行政系統:issues@staff.sitcon.org'
+DEFAULT_NOTIFICATION_SENDER = 'SITCON 行政系統:notifications@staff.sitcon.org'
+DEFAULT_ACCOUNTS_SENDER =  'SITCON 行政系統:accounts@staff.sitcon.org'
+DEFAULT_ISSUE_SENDER = 'SITCON 行政系統:issues@staff.sitcon.org'
 
-SUBMITTER_ACCOUNTS_SENDER = u'SITCON:accounts@staff.sitcon.org'
-USER_ISSUE_SENDER = u'{0} (SITCON):issues@staff.sitcon.org'
+SUBMITTER_ACCOUNTS_SENDER = 'SITCON:accounts@staff.sitcon.org'
+USER_ISSUE_SENDER = '{0} (SITCON):issues@staff.sitcon.org'
 
 DEFAULT_SMS_SENDER = 'SITCON'
 
