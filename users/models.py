a5e854b40550c041c7446b6666acaa4a8ac513ed
野生的松鼠 <kanade123321@gmail.com>
2015-09-11 02:52:11 +0800

    create Token models for registeration.

--- users/models.py
+++ users/models.py
@@ -1,71 +1,87 @@
 from django.db import models
-from django.db.models import signals
 from django.contrib.auth.models import User, Group
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.utils.crypto import get_random_string
+
 
 def photo_path(instance, filename):
     return u'photos/{} - {}'.format(
-            instance.display_name,
-            filename)
+        instance.display_name,
+        filename)
+
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, related_name='profile')
-	display_name = models.CharField(max_length=16, default='Not set')
-	title = models.CharField(max_length=16, default='Not set')
-	school = models.CharField(max_length=32, blank=True, help_text='school or company')
-	bio = models.TextField(max_length=300, help_text='biography')
-	grade = models.CharField(max_length=32, blank=True, help_text='department and grade / position')
-	phone = models.CharField(max_length=16, blank=True)
-	photo = models.FileField(upload_to=photo_path, blank=True)
-	departure = models.CharField(max_length=10, blank=True, help_text='departure')
-	comment = models.TextField(blank=True)
+    user = models.OneToOneField(User, related_name='profile')
+    display_name = models.CharField(max_length=16, default='Not set')
+    title = models.CharField(max_length=16, default='Not set')
+    school = models.CharField(max_length=32, blank=True, help_text='school or company')
+    bio = models.TextField(max_length=300, help_text='biography')
+    grade = models.CharField(max_length=32, blank=True, help_text='department and grade / position')
+    phone = models.CharField(max_length=16, blank=True)
+    photo = models.FileField(upload_to=photo_path, blank=True)
+    departure = models.CharField(max_length=10, blank=True, help_text='departure')
+    comment = models.TextField(blank=True)
 
-	def __unicode__(self):
-		return '%s - %s' % (self.title, self.user.username)
+    def __unicode__(self):
+        return '%s - %s' % (self.title, self.user.username)
 
-	def name(self):
-		if self.display_name:
-			return self.display_name
-		elif self.user.first_name and self.user.last_name:
-			return '%s %s' % (self.user.last_name, self.user.first_name)
-		else:
-			return self.user.username
+    def name(self):
+        if self.display_name:
+            return self.display_name
+        elif self.user.first_name and self.user.last_name:
+            return '%s %s' % (self.user.last_name, self.user.first_name)
+        else:
+            return self.user.username
 
-	def avatar(self):
-		if not self.photo:
-			import md5
-			hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
-			return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
-		else:
-			return reverse('users:photo_medium', kwargs={'username': self.user.username}, current_app='users')
+    def avatar(self):
+        if not self.photo:
+            import md5
+            hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
+            return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
+        else:
+            return reverse('users:photo_medium', kwargs={'username': self.user.username}, current_app='users')
 
-	def avatar_small(self):
-		if not self.photo:
-			import md5
-			hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
-			return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
-		else:
-			return reverse('users:photo_small', kwargs={'username': self.user.username}, current_app='users')
+    def avatar_small(self):
+        if not self.photo:
+            import md5
+            hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
+            return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
+        else:
+            return reverse('users:photo_small', kwargs={'username': self.user.username}, current_app='users')
 
-	def avatar_full(self):
-		if not self.photo:
-			import md5
-			hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
-			return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
-		else:
-			return self.photo.url
+    def avatar_full(self):
+        if not self.photo:
+            import md5
+            hash_value = md5.new(self.user.email.strip().lower()).hexdigest()
+            return ('https://secure.gravatar.com/avatar/%s?d=retro' % hash_value)
+        else:
+            return self.photo.url
 
-	def is_authorized(self):
-		return self.user.groups.filter(id=settings.STAFF_GROUP_ID).exists()
+    def is_authorized(self):
+        return self.user.groups.filter(id=settings.STAFF_GROUP_ID).exists()
 
-	def is_trusted(self):
-		return self.is_authorized() and self.user.has_perm('auth.change_user')
+    def is_trusted(self):
+        return self.is_authorized() and self.user.has_perm('auth.change_user')
+
 
 class GroupCategory(models.Model):
-	name = models.CharField(max_length=30)
-	is_visible = models.BooleanField(default=True)
-	groups = models.ManyToManyField(Group, related_name='categories')
+    name = models.CharField(max_length=30)
+    is_visible = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group, related_name='categories')
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+
+class RegisterToken(models.Model):
+    """
+    valid:  flag for current token can use or not
+    user:   after registration, the token will link to the user.
+            Team leader can trace the usage of tokens.
+    group:  User registered by token is belongs to.
+    """
+    token = models.CharField(max_length=12, default=get_random_string)
+    group = models.ForeignKey(GroupCategory)
+    valid = models.BooleanField(default=True)
+    user = models.ForeignKey(User, default=None, null=True)
