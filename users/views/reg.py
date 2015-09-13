7a6891e8dcab74ef0488410dc9d9d3cada0208b1
Philip Chou <pcchou@pcchou.me>
2015-09-14 06:05:07 +0800

    add empty views

---
+++ users/views/reg.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.utils import sorted_users, sorted_categories
+from django.http import HttpResponse
+
+def reg_list_token(request):
+    pass
+
+def reg_add_token(request):
+    pass
+
+def reg_form(request):
+    pass
