e02665b8c1c2d8c8617fc5d4b6eb454013a64ad8
Philip Chou <pcchou@pcchou.me>
2015-09-24 21:00:46 +0800

    Add user into groups, and login after registered w/token

--- users/views/reg.py
+++ users/views/reg.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import Group, User
 from django.http import HttpResponse, HttpResponseForbidden
 from django.db import transaction
 
-from users.models import RegisterToken
+from users.models import RegisterToken, UserProfile
 from users.utils import sorted_users, sorted_tokens, sorted_categories
 from users.forms import RegisterForm
 
@@ -74,6 +75,16 @@ def reg_form(request, token=None):
             reg_token.valid = False
             reg_token.user = form.instance
             reg_token.save()
+
+            p = UserProfile()
+            p.user = form.instance
+            p.save()
+
+            for g in reg_token.groups.all():
+                g.user_set.add(form.instance)
+
+            form.instance.backend = 'django.contrib.auth.backends.ModelBackend'
+            login(request, form.instance)
 
     return redirect('users:edit fancy', username=form.instance.username)
 
