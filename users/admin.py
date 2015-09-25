ffacf3ebfb4fa05db1104ee3a53c1854a13f70e4
Philip Chou <pcchou@pcchou.me>
2015-09-25 20:16:25 +0800

    Allow edit language and abilities in admin

--- users/admin.py
+++ users/admin.py
@@ -4,3 +4,6 @@ from users.models import *
 admin.site.register(UserProfile)
 admin.site.register(GroupCategory)
 admin.site.register(RegisterToken)
+admin.site.register(language)
+admin.site.register(abilities)
+
