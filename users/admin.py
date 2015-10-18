1efcd76f111ff472bdcaaae5f6089a45ab371c13
Pellaeon Lin <nfsmwlin@gmail.com>
2015-10-18 20:16:45 +0800

    Update# enable and disable actions in UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from users.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(UserProfile)
 admin.site.register(GroupCategory)
@@ -7,3 +9,14 @@ admin.site.register(RegisterToken)
 admin.site.register(language)
 admin.site.register(abilities)
 
+class MyUserAdmin(UserAdmin):
+    actions = ['disable', 'enable']
+
+    def disable(self, request, queryset):
+        queryset.update(is_active=False)
+
+    def enable(self, request, queryset):
+        queryset.update(is_active=True)
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
