a16fd8fb7d822a7de1afcbeac059b8c85ce0c0b9
Pellaeon Lin <nfsmwlin@gmail.com>
2015-11-29 10:13:19 +0800

    Update# display on login page if this is oauth login

--- users/views/auth.py
+++ users/views/auth.py
@@ -29,6 +29,7 @@ def login(request):
 
     url = request.REQUEST.get('next')
     context['redirect_url'] = url
+    context['oauth'] = url.startswith('/o/')
     return render(request, 'users/login.html', context)
 
 def logout(request):
