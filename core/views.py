d483d572a2fde0167f3ffcae59050a9a0e77f887
Pellaeon Lin <nfsmwlin@gmail.com>
2015-12-26 11:22:01 +0800

    Update# index page: let user view their submissions after CFP ends

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from core.settings.base import SUBMISSION_END
+import datetime
 
 @login_required
 def index(request):
     context = {}
     context['submissions'] = request.user.submissions.all()
+    context['expired'] = SUBMISSION_END < datetime.datetime.now()
 
     return render(request, 'index.html', context)
