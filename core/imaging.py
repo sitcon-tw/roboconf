c97ab46537defd39cda79902db988ff98e8ea046
Philip Chou <pcchou@pcchou.me>
2016-04-12 23:51:35 +0800

    Fix avatar upload issue caused by hashlib
    python3 hashlib, #backport
    
    (cherry picked from commit 16d6b5a07eb7df8f1b918dccaac3c1b8d213fae8)

--- core/imaging.py
+++ core/imaging.py
@@ -5,10 +5,10 @@ def resize_image(image_file, size):
     with Image(file=image_file) as image:
         # Crop to square image first
         if image.width > image.height:
-            x = (image.width - image.height) / 2
+            x = (image.width - image.height) // 2
             image.crop(left=x, width=image.height)
         elif image.height > image.width:
-            y = (image.height - image.width) / 2
+            y = (image.height - image.width) // 2
             image.crop(top=y, height=image.width)
 
         if image.width > size:
